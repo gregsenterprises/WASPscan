5550afb0cdece794a5d16a9763c2269e654746e2
Bob Mottram <bob@robotics.uk.to>
2015-04-11 18:58:19 +0100

    More parameter tweaking

--- src/detect.c
+++ src/detect.c
@@ -253,8 +253,8 @@ float detect_orbital_period(float timestamp[],
     float period_days=0;
     float max_response = 0;
     const int expected_width = 256*2/100;
-    const int max_dipped = 256*5/100;
-    const int max_nondipped = 256*5/100;
+    const int max_dipped = 256*9/100;
+    const int max_nondipped = 256*9/100;
     int step = 0;
     int steps = (int)((max_period_days - min_period_days)/increment_days);
     float response[MAX_SEARCH_STEPS];
--- src/gnuplot.c
+++ src/gnuplot.c
@@ -297,8 +297,8 @@ int gnuplot_light_curve_distribution(char * title,
 
     mean = detect_mean(series, series_length);
     variance = detect_variance(series, series_length, mean);
-    range_min = mean - variance*4;
-    range_max = mean + variance*4;
+    range_min = mean - variance*2;
+    range_max = mean + variance*2;
 
     if (gnuplot_create_script((char*)plot_script_filename,
                               (char*)plot_data_filename,
